85979c564a74deef3599b81f2c1d53b31ea188f4
Aljon C. Tocayon <actWhitehat27@gmail.com>
2020-08-19 16:26:26 +0800

    temp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useEffect} from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./styles.css"
+import NotFoundPage from "./404"
 function App() {
+
+    useEffect( () => {
+        localStorage.setItem("time", Date.now());
+    });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <Switch>
+          <Route path={"/document-tracking"} exact />
+          <Route path={"/work-queue"} exact render={() => {
+              window.location = "http://localhost:5003"
+          }} />
+            <Route component={NotFoundPage} />
+        </Switch>
+      </div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Router>
+          <Route component={App} />
+      </Router>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
